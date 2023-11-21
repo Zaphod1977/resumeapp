dd8b388efea056f87b33f0ab51a2ef6e468a0f38
carfiguru@hotmail.com <carfiguru@hotmail.com>
2023-11-21 10:39:03 -0600

    added youtube link

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ function About() {
     <section id="about" className="aboutme">
       <h2 id="about" className="aboutme-leftside">About</h2>
       <p className="aboutme-rightside">
-            My name is Bobby Bridges. I am a 46 year old father, business owner, and now full stack developer. I have
+            My name is Bobby Bridges. I am a 47 year old father, business owner, and now full stack developer. I have
             been in the tech world since I was a kid taking
             audio gear apart to figure out how it worked and how to make it do different things I wanted to make it do.
             That engenuity brought me to the audio/video and automation
@@ -13,7 +13,11 @@ function About() {
             learn how to write code and to discover the underpinnings of the computing world I so richly appreciate. So
             here I am...46 and learning to code. The next chapter will
             be to discover the new opportunities my newly earned skills will present me. Hello World.
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/mgkYIXeXOBk?si=6p4C8e-K64v9Yvg7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
         </p>
+        <div>
+        </div>
     </section>
   );
 }
