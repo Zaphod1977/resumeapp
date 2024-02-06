f833612bd54b5294644f6a09fe083a8114db97f5
carfiguru@hotmail.com <carfiguru@hotmail.com>
2024-02-05 22:00:37 -0600

    sorted out some styling improvements and cleaned up a few things

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,21 +3,22 @@ function About() {
   return (
     <section id="about" className="aboutme">
       <h2 id="about" className="aboutme-leftside">About</h2>
-      <p className="aboutme-rightside">
-            My name is Bobby Bridges. I am a 47 year old father, business owner, and now full stack developer. I have
-            been in the tech world since I was a kid taking
-            audio gear apart to figure out how it worked and how to make it do different things I wanted to make it do.
-            That engenuity brought me to the audio/video and automation
-            world back in July of 2000. From there I have rose to be one of the leading integrators in my region and
-            also one of the most respected. But.....I've always wanted to
-            learn how to write code and to discover the underpinnings of the computing world I so richly appreciate. So
-            here I am...46 and learning to code. The next chapter will
-            be to discover the new opportunities my newly earned skills will present me. Hello World.
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/mgkYIXeXOBk?si=6p4C8e-K64v9Yvg7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
-        </p>
-        <div>
-        </div>
+      <div className="aboutme-rightside-about">
+        My name is Bobby Bridges. I am a 47 year old father, business owner, and now full stack developer.
+        I have been in the tech world since I was a kid taking audio gear apart to figure out how it
+        worked and how to make it do different things I wanted to make it do. That engenuity brought me
+        to the audio/video and automation world back in July of 2000. From there I have rose to be one
+        of the leading integrators in my region and also one of the most respected. But.....I've always
+        wanted to learn how to write code and to discover the underpinnings of the computing world I so
+        richly appreciate. So here I am...47, coding daily & working towards proficiency. The next
+        chapter will be to discover the new opportunities my newly earned skills will present me.
+        <br></br><br></br>Hello World.
+        <br></br><br></br>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/mgkYIXeXOBk?si=6p4C8e-K64v9Yvg7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+      </div>
+      <br></br><br></br><br></br><br></br>
     </section>
   );
 }
